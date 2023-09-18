b7a34ac6793657df0546da6ed93dd5a6a1b240bc
ahgaming03 <85728869+ahgaming03@users.noreply.github.com>
2023-09-18 14:14:51 +0700

    upload server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3333
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 9999;
 
-app.get('/', (req, res) => res.send('Test render!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => {
+    res.send(
+        fs.readFileSync("./index.html", { encoding: "utf-8" })
+    );
+    
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
